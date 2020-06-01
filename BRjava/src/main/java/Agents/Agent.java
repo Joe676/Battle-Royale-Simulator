f4dde0caaffc68e263892ee95365b2a570fa2499
JB <43980398+Joe676@users.noreply.github.com>
2020-06-01 17:40:51 +0200

    Agent Javadoc Comments
    Added javadoc comments to the agent class

--- BRjava/src/main/java/Agents/Agent.java
+++ BRjava/src/main/java/Agents/Agent.java
@@ -13,6 +13,11 @@ import Vectors.Raycast;
 import Vectors.Vector;
 import Wall.Wall;
 
+/**
+ * Class representing a player
+ * @author Jozef Bossowski
+ *
+ */
 public class Agent{
 	
 	private int ID;
@@ -71,6 +76,14 @@ public class Agent{
 	
 	
 	//constructors
+	/**
+	 * Constructs and initializes an Agent object
+	 * 
+	 * Creates an agent at a specified position of given map and sets its identification number. 
+	 * @param pos Vector position at which agent is initialized
+	 * @param map A map at which the agent is created. Used for references to other objects on the map
+	 * @param id Identification number. Used for printing agent
+	 */
 	public Agent(Vector pos, Map map, int id) {
 		this.ID = id;
 		this.pos = pos.copy();
@@ -80,17 +93,36 @@ public class Agent{
 		
 		this.angle = Math.random()*360 - 180;
 	}
-	
+	/**
+	 * Constructs and initializes an Agent object
+	 * 
+	 * Creates an agent at a specified position of given map and sets its identification number. 
+	 * @param x X coordinate of position at which agent is initialized
+	 * @param y Y coordinate of position at which agent is initialized
+	 * @param map A map at which the agent is created. Used for references to other objects on the map
+	 * @param id Identification number. Used for printing agent
+	 */
 	public Agent(double x, double y, Map map, int id) {
 		this(new Vector(x, y), map, id);
 	}
 	
 	//getters & setters
+	/**
+	 * Sets agent's position to a specified (x, y) location
+	 * @param x X coordinate 
+	 * @param y Y coordinate
+	 */
 	public void setPos(double x, double y) {
 		this.pos.setX(x);
 		this.pos.setY(y);
 	}
 	
+	/**
+	 * Sets agent's weapon to the specified one
+	 * 
+	 * Switches weapons if it's not the first picked up
+	 * @param weapon Weapon object to be set as new weapon
+	 */
 	public void setWeapon(Weapon weapon) {
 		if(this.weapon!=null) {
 			this.dropWeapon();
@@ -98,15 +130,28 @@ public class Agent{
 		this.weapon = weapon;
 	}
     
+	/**
+	 * Returns agent's weapon
+	 * @return Agent's Weapon object 
+	 */
 	public Weapon getWeapon() {
 		return this.weapon;
 	}
 	
+	/**
+	 * Returns static value of Agent's visual diameter
+	 * @return Static Agent diameter
+	 */
 	public static double getR() {
 		return Agent.R;
 	}
 	
 	//calculating bounding circle position for drawing 
+	/**
+	 * Calculates agent's center after movement
+	 * 
+	 * Calculates center of the agent and bounding circle's position for drawing reference
+	 */
     private void calcBounds() {
     	this.CENTER = this.pos.copy();
     	this.CENTER.add(new Vector(Agent.R/2, Agent.R/2));
@@ -115,34 +160,67 @@ public class Agent{
     	this.boundingCirclePos.sub(new Vector(this.boundingCircleR, this.boundingCircleR));
     }
 	
+    /**
+     * 
+     * @return Copy of Vector position
+     */
 	public Vector getPos() {
 		return this.pos.copy();
 	}
 	
+	/**
+	 * 
+	 * @return Copy of agent's Vector Center
+	 */
 	public Vector getCENTER() {
 		return this.CENTER.copy();
 	}
 	
+	/**
+	 * 
+	 * @return Radius of bounding circle
+	 */
 	public double getBoundingCircleR() {
 		return this.boundingCircleR;
 	}
 
+	/**
+	 * 
+	 * @return Angle of vision arc
+	 */
 	public double getVisionAngle() {
 		return this.visionAngle;
 	}
-
+	
+	/**
+	 * 
+	 * @return Radius of vision arc
+	 */
 	public double getVisionR() {
 		return this.visionR;
 	}
 	
+	/**
+	 * 
+	 * @return Agent's maximum HP
+	 */
 	public double getMaxHP() {
 		return this.maxHP;
 	}
 	
+	/**
+	 * 
+	 * @return Agent's current HP
+	 */
 	public double getHP() {
 		return this.HP;
 	}
 	
+	/**
+	 * Makes a decision what to do
+	 * 
+	 * Based on current game state decides what an agent should do
+	 */
 	public void decide(){
 		Item prev = null;
 		if(this.HP <= 30) {
@@ -186,6 +264,10 @@ public class Agent{
 	}
 	
 	//rotate towards some point
+	/**
+	 * Changes agent's angle to face a specified point
+	 * @param target point to be faced by the agent
+	 */
 	public void rotateSeek(Vector target) {
 		Vector desire = target.copy();
 		desire.sub(this.pos);
@@ -197,6 +279,9 @@ public class Agent{
 	}
 	
 	// rotate in a random pattern (idle)
+	/**
+	 * Rotates agent in a randomised pattern when idle
+	 */
 	public void rotateWander() {
 		this.angle+=this.rotateSpeed;
 		this.rotateSpeed += Math.random()*2*this.RSPEED_CHANGE - this.RSPEED_CHANGE;
@@ -205,12 +290,23 @@ public class Agent{
 	}
 	
 	//steering behaviours
+	/**
+	 * Applies the steering force
+	 * @param force Steering force
+	 */
 	private void steer(Vector force) {
 		force.constrain(this.maxForce);
 		this.acc.add(force);
 	}
 	
 	//go towards a point
+	/**
+	 * Moves an agent towards a specified point
+	 * 
+	 * Inspired by http://www.red3d.com/cwr/steer/SeekFlee.html
+	 * @param target Target to which agent has to go
+	 * @param weight Specifies how important is the movement
+	 */
 	public void seek(Vector target, double weight) {
 		Vector desire = new Vector();
 		desire.add(target);
@@ -224,11 +320,23 @@ public class Agent{
 	}
 	
 	//run from the point
+	/**
+	 * Moves an agent from a specified point as if it was running away
+	 * 
+	 * Inspired by http://www.red3d.com/cwr/steer/SeekFlee.html
+	 * @param target Point from which to flee
+	 * @param weight Specifies how important is the movement
+	 */
 	public void flee(Vector target, double weight) {
 		this.seek(target, -weight);
 	}
 	
 	//move in a random pattern (idle)
+	/**
+	 * Moves an agent in a randomised pattern
+	 * 
+	 * Imitates realistic movement. Inspired by http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.86.636&rep=rep1&type=pdf
+	 */
 	public void wander() {
 		Vector circleCenter = this.vel.copy();
 		circleCenter.normalize();
@@ -245,6 +353,10 @@ public class Agent{
 	}
 	
 	//not run into a wall
+	/**
+	 * Keeps an agent from running into a wall
+	 * @param weight Specifies how important is the movement
+	 */
 	public void avoidObstacles(double weight) {
 		List<Wall> intersected = this.getIntersectedWalls();
 		Vector force;
@@ -260,6 +372,9 @@ public class Agent{
 	}	
 
 	//update own movement variables
+	/**
+	 * Updates the movement variables to simulate physics
+	 */
 	public void update() {
 		this.vel.add(this.acc);
 		this.vel.constrain(this.maxSpeed);
@@ -273,6 +388,12 @@ public class Agent{
 	}
 	
 	//Items
+	/**
+	 * Tries to pick up a specified item
+	 * 
+	 * If the item is out of reach but visible goes towards it
+	 * @param item Item to be picked up
+	 */
 	public void pickUp(Item item) {
 		if(item==null)return;
 		if(Vector.pointDistance(item.getPos(), this.getCENTER())<Agent.getR()) {
@@ -283,6 +404,9 @@ public class Agent{
 		}
 	}
 	
+	/**
+	 * Leaves weapon on the map at the current location
+	 */
 	private void dropWeapon() {
 		this.weapon.setIsOnMap(true);
 		this.weapon.setPos(this.pos.copy());
@@ -290,6 +414,9 @@ public class Agent{
 		this.weapon = null;
 	}
 	
+	/**
+	 * Leaves armor on the map at the current location
+	 */
     private void dropArmor() {
     	System.out.println("dropping armor");
     	this.armor.setIsOnMap(true);
@@ -298,6 +425,10 @@ public class Agent{
     	this.armor = null;
 	}
 
+    /**
+     * Returns items that an agent is able to pick up
+     * @return List of items in range
+     */
     private List<Item> getItemsInRange(){
     	List<Item> I = this.map.getItems();
     	List<Item> out = new ArrayList<Item>();
@@ -312,6 +443,11 @@ public class Agent{
     	return out;
     }
     
+    /**
+     * Checks whether given item is within visibility arc and not obstructed by a wall
+     * @param item Item to raycast to
+     * @return true if item is visible, false otherwise
+     */
     private boolean checkItemVisibility(Item item) {
 		Vector diff = item.getPos();
 		diff.sub(this.getCENTER());
@@ -336,6 +472,10 @@ public class Agent{
 		return false;
 	}
     
+    /**
+     * Returns items that are within field of view and are not obstructed 
+     * @return List of visible items
+     */
     private List<Item> getVisibleItems(){
     	List<Item> I = this.map.getItems();
     	List<Item> out = new ArrayList<Item>();
@@ -347,6 +487,10 @@ public class Agent{
     	return out;
     }
     
+    /**
+     * Checks whether a weapon is in range of picking up or in the field of view
+     * @return A weapon if found, null otherwise
+     */
     public Weapon lookForWeapon() {
     	List<Item> I = this.getItemsInRange();
     	for(Item item: I) {
@@ -363,6 +507,10 @@ public class Agent{
     	return null;
     }
     
+    /**
+     * Checks whether a first aid kit is in range of picking up or in the field of view
+     * @return FirstAidKit if found, null otherwise
+     */
     public FirstAidKit lookForFirstAidKit() {
     	List<Item> I = this.getItemsInRange();
     	for(Item item: I) {
@@ -379,6 +527,10 @@ public class Agent{
     	return null;
     }
     
+    /**
+     * Checks whether a first aid kit is in range of picking up or in the field of view
+     * @return Armor if found, null otherwise
+     */
     public Armor lookForArmor() {
     	List<Item> I = this.getItemsInRange();
     	for(Item item: I) {
@@ -395,12 +547,23 @@ public class Agent{
     	return null;
     }
     
+    /**
+     * Returns distance from an agent to a given wall
+     * @param wall Wall to check distance to
+     * @return distance to specified wall
+     */
  	public double getDistanceToWall(Wall wall) {
     	Vector pt = wall.getClosestPoint(this.CENTER);
     	pt.sub(this.CENTER);
     	return pt.magnitude();
     }
     
+ 	/**
+ 	 * Returns the closest points of all walls
+ 	 * 
+ 	 * For each wall returns the point closest to the agent, but still on the wall
+ 	 * @return List of closest points on the walls 
+ 	 */
     public List<Vector> getClosestWallPoints() {
     	List<Vector> V = new ArrayList<Vector>();
 		List<Wall> W = this.map.getWalls();
@@ -411,10 +574,19 @@ public class Agent{
     }
     
     //check if wall in bounds
+    /**
+     * Checks whether specified wall intersects agent's bounding circle. Used for evading obstacles
+     * @param wall A wall to be checked
+     * @return true if intersects, false otherwise
+     */
     public boolean checkWallIntersection(Wall wall) {//true - intersects
     	return this.getDistanceToWall(wall) <= this.boundingCircleR;
     }
     
+    /**
+     * Returns walls that intersect agent's bounding circle
+     * @return List of walls that are too close
+     */
     public List<Wall> getIntersectedWalls(){
 		List<Wall> W = this.map.getWalls();
     	List<Wall> out = new ArrayList<Wall>();
@@ -425,31 +597,22 @@ public class Agent{
     	}
     	return out;
     }
-    
-    public List<Agent> getIntersectedAgents(){
-		List<Agent> A = this.map.getAgents();
-    	List<Agent> out = new ArrayList<Agent>();
-    	for(Agent agent: A) {
-    		if(this.checkAgentIntersection(agent)) {
-    			out.add(agent);
-    		}
-    	}
-    	return out;
-    }
-    
-
-	private boolean checkAgentIntersection(Agent agent) {//true - intersects
-    	return this.getDistanceToAgent(agent) <= this.boundingCircleR;
-	}
-
 	
+    /**
+     * Returns distance to a specified agent
+     * @param agent Agent to whom distance is to be calculated
+     * @return distance between the agents
+     */
 	public double getDistanceToAgent(Agent agent) {
 		Vector pt = agent.getCENTER();
 		pt.sub(this.CENTER);
 		return pt.magnitude();
 	}
 
-	
+	/**
+	 * returns agents in the field of view 
+	 * @return List of visible agents
+	 */
 	public List<Agent> getVisibleAgents(){
 		List<Agent> A = this.map.getAgents();
 		List<Agent> out = new ArrayList<Agent>();
@@ -463,6 +626,11 @@ public class Agent{
 		return out;
 	}
 	
+	/**
+	 * Checks whether a specified agent is within field of view and isn't obstructed by a wall
+	 * @param agent Agent to check visibility to
+	 * @return true if agent is visible, false otherwise
+	 */
 	public boolean checkAgentVisibility(Agent agent) {
 		Vector diff = agent.getCENTER();
 		diff.sub(this.getCENTER());
@@ -487,6 +655,9 @@ public class Agent{
 		return false;
 	}
 	
+	/**
+	 * Tries to shoot
+	 */
 	public void shoot() {
 		if(this.weapon != null) {
 			this.weapon.shoot();
@@ -494,6 +665,11 @@ public class Agent{
 	}
 	
 	//check if shot landed on agent
+	/**
+	 * Checks whether the shot landed
+	 * @param angleOffset angle difference between the shot and agent's facing angle
+	 * @param damage damage dealt to the hit agent
+	 */
 	public void checkShot(double angleOffset, int damage) {
 		Vector dir = new Vector(1, 1);
 		dir.fromAngle(1, Math.toRadians(this.angle + angleOffset));
@@ -525,10 +701,20 @@ public class Agent{
 		
 	}
 	
+	/**
+	 * Returns agent's armor 
+	 * @return Armor of this agent
+	 */
 	public Armor getArmor() {
 		return this.armor;
 	}
 	
+	/**
+	 * Sets agent's armor to a specified one
+	 * 
+	 * Switches if not the first
+	 * @param armor Armor to be set as agent's
+	 */
 	public void setArmor(Armor armor) {
 		if(this.armor!=null && armor!=null) {
 			this.dropArmor();
@@ -536,6 +722,12 @@ public class Agent{
 		this.armor = armor;
 	}
 	
+	/**
+	 * Damages agent on hit
+	 * 
+	 * If agent wears armor damages armor instead
+	 * @param damage Amount of damage dealt 
+	 */
 	public void hit(int damage) {
 		if(this.armor!=null) {
 			this.armor.hit(damage);
@@ -547,11 +739,17 @@ public class Agent{
 		}
 	}
 	
+	/**
+	 * The agent got killed
+	 */
 	public void die() {
 		this.map.agentDied(this);
 	}
 
 	//drawing
+	/**
+	 * Draws this agent on the canvas
+	 */
     public void show(Graphics g){
         final int x = (int)this.pos.getX();
         final int y = (int)this.pos.getY();
@@ -569,13 +767,17 @@ public class Agent{
         g.setColor(new Color(30, 30, 30, 10));
         g.fillArc((int)(center.getX()-this.visionR), (int)(center.getY()-this.visionR), (int)(2*this.visionR), (int)(2*this.visionR), -(int)(this.angle-this.visionAngle/2), -(int)this.visionAngle);
     }
-
+    
+    /**
+     * Heals the agent a specified amount
+     * @param hp Health Points to be healed
+     */
 	public void heal(int hp) {
 		this.HP += hp;
 		if(this.HP>this.maxHP)this.HP = this.maxHP;
 	}
 	
-    
+	@Override
 	public String toString() {
 		return "Agent #"+this.ID;
 	}
